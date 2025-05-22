6c53a52f5877392212be2f0019095754b99bd8db
Felix Jarquin <fjarq005@fiu.edu>
2025-05-22 03:51:31 -0400

    obj spawning
    my objects seem to spawn in but arent given gravity despite being told to do so

--- Assets/Scripts/GivebackGrav.cs
+++ Assets/Scripts/GivebackGrav.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
 
 public class GivebackGrav : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+{ 
+    [SerializeField] private GameObject prefab1;
+    [SerializeField] private GameObject prefab2;
+    [SerializeField] private Transform spawnPoint;
 
-    // Update is called once per frame
-    void Update()
+    public void SpawnObject1()
     {
+        SpawnPrefab(prefab1);
+    }
+    
+    public void SpawnObject2()
+    {
+        SpawnPrefab(prefab2);
+    }
+    
+    private void SpawnPrefab(GameObject prefabToSpawn)
+    {
+        GameObject newObject = Instantiate(prefabToSpawn, spawnPoint.position, spawnPoint.rotation);
+        newObject.transform.SetParent(null);
         
+        Rigidbody rb = newObject.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = false;
+            rb.useGravity = true;
+        }
     }
 }
