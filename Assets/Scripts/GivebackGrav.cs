f23236757e5c33193dc8e157f3b28be2740010b1
Felix Jarquin <fjarq005@fiu.edu>
2025-05-22 04:20:54 -0400

    Test1
    If this dont work ima be mad

--- Assets/Scripts/GivebackGrav.cs
+++ Assets/Scripts/GivebackGrav.cs
@@ -1,31 +1,28 @@
 using UnityEngine;
-
 public class GivebackGrav : MonoBehaviour
 { 
     [SerializeField] private GameObject prefab1;
     [SerializeField] private GameObject prefab2;
     [SerializeField] private Transform spawnPoint;
-
     public void SpawnObject1()
-    {
-        SpawnPrefab(prefab1);
-    }
-    
+    {SpawnPrefab(prefab1);}
     public void SpawnObject2()
-    {
-        SpawnPrefab(prefab2);
-    }
-    
+    {SpawnPrefab(prefab2);}
     private void SpawnPrefab(GameObject prefabToSpawn)
     {
-        GameObject newObject = Instantiate(prefabToSpawn, spawnPoint.position, spawnPoint.rotation);
-        newObject.transform.SetParent(null);
-        
+        Vector3 worldPosition = spawnPoint.position;
+        Quaternion worldRotation = spawnPoint.rotation;
+        GameObject newObject = Instantiate(prefabToSpawn, worldPosition, worldRotation, null);
+        Debug.Log("Spawned object: " + newObject.name);
         Rigidbody rb = newObject.GetComponent<Rigidbody>();
-        if (rb != null)
-        {
-            rb.isKinematic = false;
-            rb.useGravity = true;
+        Debug.Log("Rigidbody found: " + (rb != null));
+        
+        if (rb == null)
+        {Debug.Log("Adding Rigidbody...");
+         rb = newObject.AddComponent<Rigidbody>();
         }
+        rb.isKinematic = true;
+        rb.useGravity = false;
+        Debug.Log("Rigidbody set to kinematic: " + rb.isKinematic);
     }
 }
